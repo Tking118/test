2275d9967824260caabf05d2c483f8eed82874d1
Tking118 <t.kosoko1000@web.de>
2026-04-23 11:34:44 +0200

    test

---
+++ Mathe.java
@@ -0,0 +1,19 @@
+public class Mathe {
+    public static int max (int a ,int b){
+        if (a>=b) 
+            {return a;}
+        else 
+            {return b;}
+    }
+    public static String signum(int n) {
+        if ( n < 0 ) 
+            {return "negativ";}
+        else 
+            if ( n > 0 )
+                {return "positiv ";}
+            else
+            {return "natural ";}
+     }
+    
+     
+}
